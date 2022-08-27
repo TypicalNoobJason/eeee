d8b80fda6b2bb8a8fc8ea4452b1e5cef28aa938d
TypicalNoobJason <99631512+TypicalNoobJason@users.noreply.github.com>
2022-08-26 19:46:40 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("myfirstconsoleapp");
+var first_name="BOB";
+var last_name="GRAND";
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+var a=10;
+var b=5;
+var add=a+b;
+console.log(add);
+var subtract=a-b;
+console.log(subtract);
+var multiply=a*b;
+console.log(multiply);
+var divide=a/b;
+console.log(divide);
+
+
+
+
+
+
